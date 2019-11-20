a68dcf5f77e9ea143c58df7ddf96a064f5e10bfd
James Irwin <rebolyte@gmail.com>
2019-11-20 14:23:42 -0600

    format error responses as well

--- packages/api/src/middleware/error.middleware.ts
+++ packages/api/src/middleware/error.middleware.ts
@@ -2,10 +2,10 @@ import { ErrorRequestHandler } from 'express';
 
 const clientErrorHandler: ErrorRequestHandler = (err, _req, res, next) => {
 	if (err.name === 'UnauthorizedError') {
-		res.status(401).send(err.inner);
+		res.status(401).json(err.inner);
 		// Note that we're not passing on this "error" to Winston
 	} else {
-		res.status(500).send(err);
+		res.status(500).json(err);
 		next(err);
 	}
 };
