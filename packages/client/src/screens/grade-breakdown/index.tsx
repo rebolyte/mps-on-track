831e4b5d940b774fcce1a4cdabbdf27dd5c1551e
James Irwin <rebolyte@gmail.com>
2019-12-19 10:50:26 -0600

    add real courses with credits table

--- packages/client/src/screens/grade-breakdown/index.tsx
+++ packages/client/src/screens/grade-breakdown/index.tsx
@@ -14,6 +14,8 @@ import { useStores } from '../../stores';
 import { Async, uniqueIdRandom } from '../../utilities';
 import { Spinner, DataTable } from '../../components';
 
+import './grade-breakdown.css';
+
 interface GradeTableProps {
 	data: StudentGradeBreakdownResponse;
 }
@@ -70,12 +72,54 @@ const GradeTable: FC<GradeTableProps> = observer(({ data }: GradeTableProps) => 
 	);
 });
 
+interface Item {
+	name: string;
+	type: 'schoolYear' | 'grade' | 'courseDetail';
+	[key: string]: any;
+}
+
 const StudentCourseCredits: FC<StudentCourseCreditsProps> = observer(
 	({ data }: StudentCourseCreditsProps) => {
+		const tableData: Item[] = data.Items.reduce((acc, cur) => {
+			const schoolYear: Item = { name: cur.SchoolYear, type: 'schoolYear' };
+
+			const gradeDetails = cur.GradeDetails.reduce((gradeAcc, gradeCur) => {
+				const grade: Item = { name: gradeCur.Grade, type: 'grade' };
+
+				const courses: Item[] = gradeCur.Courses.map(({ CourseDetails, ...rest }) => ({
+					name: CourseDetails,
+					...rest,
+					type: 'courseDetail'
+				}));
+
+				return [...gradeAcc, grade, ...courses];
+			}, [] as any);
+
+			return [...acc, schoolYear, ...gradeDetails];
+		}, [] as any);
+
+		const styleRow = ({ type }: Item) => {
+			const classes = {
+				schoolYear: 'course-credits-school-year',
+				grade: 'course-credits-grade',
+				courseDetail: 'course-credits-course-detail'
+			};
+			return classes[type];
+		};
+
 		return (
 			<>
 				<h2 className="font-bold text-lg">Courses with Credits Breakdown</h2>
-				<pre>{JSON.stringify(data, null, 2)}</pre>
+				<DataTable
+					cols={[
+						{ id: 'name', title: '' },
+						{ id: 'Credits', title: 'Counts' }
+					]}
+					data={tableData}
+					idProp={row => row.name}
+					striped={false}
+					styleRow={styleRow}
+				/>
 			</>
 		);
 	}
