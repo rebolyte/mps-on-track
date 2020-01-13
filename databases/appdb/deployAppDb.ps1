e9f67d995f5834eaf53f1d773026dbe2ec486290
James Irwin <rebolyte@gmail.com>
2020-01-13 15:29:13 -0600

    use app DB

--- databases/appdb/deployAppDb.ps1
+++ databases/appdb/deployAppDb.ps1
@@ -2,12 +2,13 @@ param(
 	[string]$Environment = "PROD"
 )
 
-$connStr = "Server=localhost,1401;User Id=SA;Password=HotSprocket123!;"
+$connStr = "Server=localhost,1401;Database=OnTrackDb;User Id=SA;Password=HotSprocket123!;"
 
-rh --connectionstring $connStr --donotcreatedatabase --databasetype=sqlserver --silent --files "." --environment $Environment --warnandignoreononetimescriptchanges
+rh --connectionstring $connStr --databasetype=sqlserver --silent --files "." --environment $Environment --warnandignoreononetimescriptchanges
 
 if ($? -eq $True) {
 	Write-Host "Initialized application database"
-} else {
+}
+else {
 	Write-Host "RoundhousE error occurred"
 }
