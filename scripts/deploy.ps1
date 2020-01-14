214afdf46d6f3bea779af1fe214643698f1d11ac
James Irwin <rebolyte@gmail.com>
2020-01-14 17:44:40 -0600

    update deploy

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -4,7 +4,9 @@ $artifactDir = Join-Path $stageDir "artifact"
 
 $prodConfig = ".\ontrack.config.json"
 
-$deployDir = Join-Path $env:SystemDrive "ontrack"
+$deployDir = Join-Path $env:SystemDrive "inetpub\wwwroot"
+$apiDeployDir = Join-Path $deployDir "OnTrackApi"
+$clientDeployDir = Join-Path $deployDir "OnTrackClient"
 
 Write-Host "Looking for $($artifactZip)"
 
@@ -21,20 +23,22 @@ if (Test-Path $artifactDir) {
 
 # Grab permanent config file (which contains connstring, etc), copy to deploy
 Write-Host "Deploying $($prodConfig)..."
-Copy-Item $prodConfig $deployDir -Force
+if (!(Test-Path $apiDeployDir)) {
+	New-Item -Path $apiDeployDir -Type Directory
+}
+Copy-Item $prodConfig $apiDeployDir -Force
 
 # Unzip TeamCity artifact that has been manually uploaded to the staging directory
 Write-Host "Unzipping archive..."
 Expand-Archive $artifactZip -DestinationPath $artifactDir
 
 Write-Host "Deploying API files..."
-$apiDir = Join-Path $artifactDir "api"
-Copy-Item $apiDir $deployDir -Recurse -Force
+$apiDir = Join-Path $artifactDir "api\*"
+Copy-Item $apiDir $apiDeployDir -Recurse -Force
 
 Write-Host "Deploying web client files..."
-$clientDir = Join-Path $artifactDir "client\*"
-$clientOut = Join-Path $deployDir "wwwroot"
-Copy-Item $clientDir $clientOut -Recurse -Force
+$clientDir = Join-Path $artifactDir "client"
+Copy-Item $clientDir $clientDeployDir -Recurse -Force
 
 # call RoundhousE to run all migrations
 Write-Host "Running RoundhousE..."
@@ -49,6 +53,7 @@ Set-Location (Join-Path $artifactDir "databases\appdb")
 & .\deployAppDb.ps1 -ConnStr $appDbConn -Environment "PROD"
 
 if ($? -ne $True) {
+	Set-Location $cwd
 	exit 1
 }
 
@@ -60,7 +65,7 @@ $pm2Check = & pm2 id ontrack
 
 if ($pm2Check -eq "[]") {
 	Write-Host "Starting Node API..."
-	$api = Join-Path (Join-Path $deployDir "api") "index.js"
+	$api = Join-Path $apiDeployDir "index.js"
 	pm2 start -i 2 --name ontrack $api
 	pm2 save
 }
